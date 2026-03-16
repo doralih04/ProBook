196d05ad08755b38a1d01c633d74b73e30b68b0b
Dorali <doralihernandez04@gmail.com>
2026-03-16 16:01:05 -0600

    ProBook
    -Estructura base del proyecto con carpetas principales
    -Layouts básicos para público, manager e invitados con estilos premium
    -Configuración de rutas y conexiones necesarias
    -Colores Verde Oscuro, Oro y Arena con fuentes elegantes
    -Proyecto listo para agregar funcionalidades

---
+++ src/app/layouts/guest-layout/guest-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-guest-layout',
+  imports: [RouterModule],
+  templateUrl: './guest-layout.html',
+  styleUrl: './guest-layout.css',
+})
+export class GuestLayout {}
